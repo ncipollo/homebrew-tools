4956dbde26c3103437cd47a4f0fd13b38439b6b7
Nick Cipollo <nick.cipollo@grubhub.com>
2020-02-03 16:56:57 -0500

    Remove revision

--- Formula/tix.rb
+++ Formula/tix.rb
@@ -2,7 +2,6 @@ class Tix < Formula
     desc "A command line utility for generating jira, etc tickets from a markdown document."
     url  "https://github.com/ncipollo/tix.git",
         :tag => "0.0.2"
-        :revision => "431cfc3330fe0913603355611fbd0725230cefd7"
 
     depends_on "go" => :build
     depends_on "make" => :build
