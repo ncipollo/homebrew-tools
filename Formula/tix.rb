62417b1f2bceab6ded1464d37f1b5b6727ba09b3
Nick Cipollo <nick.cipollo@grubhub.com>
2020-02-03 16:29:17 -0500

    Add initial formula

---
+++ Formula/tix.rb
@@ -0,0 +1,17 @@
+class Docker < Formula
+    desc "A command line utility for generating jira, etc tickets from a markdown document."
+    url  "https://github.com/ncipollo/tix",
+        :tag => "0.0.2"
+
+    depends_on "go" => :build
+    depends_on "make" => :build
+
+    def install
+        ENV["GOPATH"] = buildpath
+        dir = buildpath/"src/github.com/ncipollo/tix"
+        cd bin_path do
+            system "pwd"
+            system "make build"
+        end
+    end
+end
