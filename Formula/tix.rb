1238cda05d1a375274dca5212a35139702dd7771
Nick Cipollo <nick.cipollo@grubhub.com>
2020-02-03 16:58:09 -0500

    Remove go command

--- Formula/tix.rb
+++ Formula/tix.rb
@@ -13,7 +13,6 @@ class Tix < Formula
         # to $GOPATH/src/github.com/ncipollo/tix
         src_path.install Dir["*",".??*"]
         cd src_path do
-            system "go", "build"
             system "make", "build"
         end
     end
