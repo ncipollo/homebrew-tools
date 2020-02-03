222aa494638db28a4252aa6f04ff5e3d1df45d19
Nick Cipollo <nick.cipollo@grubhub.com>
2020-02-03 18:39:28 -0500

    try again

--- Formula/tix.rb
+++ Formula/tix.rb
@@ -13,8 +13,8 @@ class Tix < Formula
         # to $GOPATH/src/github.com/ncipollo/tix
         src_path.install Dir["*",".??*"]
         cd src_path do
-            system "make", "deps"
-            system "make", "build"
+            system "make", "brew"
+            # system "make", "build"
             build_path = src_path/"build/release/tix"
             bin.install build_path
         end
