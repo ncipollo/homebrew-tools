c654569c11fc6a27dd47efff982a2a77105e4b6b
Nick Cipollo <njc1115@gmail.com>
2023-06-04 13:34:47 -0400

    Add Kotlin tix formula

---
+++ Formula/kotlin-tix.rb
@@ -0,0 +1,20 @@
+class KotlinTix < Formula
+    desc "A command line utility for generating jira, etc tickets from a markdown document."
+    url  "https://github.com/ncipollo/tix-cli.git", :tag => "0.0.1"
+
+    depends_on "openjdk@11" => :build
+
+    def install
+        java_home = Language::Java.java_home_env("11")[:JAVA_HOME]
+        ENV["JAVA_HOME"] = java_home
+        ENV["TIX_INSTALL_PATH"] = buildpath
+        system "./gradlew install"
+        bin.install buildpath/"tix"
+
+        zsh_completion.install 'completions/zsh/_tix'
+    end
+
+    test do 
+        assert_match "🚀 tix version #{version}", shell_output("#{bin}/tix --version")
+    end
+end
